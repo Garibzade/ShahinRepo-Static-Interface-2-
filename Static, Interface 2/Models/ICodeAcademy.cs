61662d382dba3e4b6b3530eec613bed4a9a05cdb
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-09 06:19:45 +0400

    Add files via upload

---
+++ Static, Interface 2/Models/ICodeAcademy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static__Interface_2.Models
+{
+    internal class ICodeAcademy
+    {
+    }
+}
---
+++ Static, Interface 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Static__Interface_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
